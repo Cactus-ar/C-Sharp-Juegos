e5022229b9305b1a98ab2c829af0a07f68a0fcc5
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2019-10-14 19:59:46 -0300

    clase ficha agregada

---
+++ CuatroEnLinea/CuatroEnLinea/Ficha.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CuatroEnLinea
+{
+    public class Ficha
+    {
+
+        public enum Jugador
+        {
+            blanco,
+            rojo,
+            amarillo
+        };
+
+
+        public Ellipse forma;
+        public Point puntos { get; set; }
+        public int Columna { get; set; }
+        public int Fila { get; set; }
+        public Jugador jugador { get; set; }
+        public string Nombre { get; set; }
+
+        public Ficha()
+        {
+            puntos = new Point();
+            forma = new Ellipse
+            {
+                Fill = new SolidColorBrush(Colors.White),
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 2
+            };
+
+            jugador = Jugador.blanco;
+
+        }
+
+    }
+}
--- CuatroEnLinea/CuatroEnLinea/MainWindow.xaml.cs
+++ CuatroEnLinea/CuatroEnLinea/MainWindow.xaml.cs
@@ -5,12 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CuatroEnLinea
@@ -21,54 +17,98 @@ namespace CuatroEnLinea
     public partial class MainWindow : Window
     {
 
-
+        List<Ficha> tablero;
+        Ficha fichin;
+        SolidColorBrush blanco;
+        SolidColorBrush rojo;
+        SolidColorBrush amarillo;
         
-        Ellipse fichin;
+        bool turno = true; //comienza el rojo
+
 
         public MainWindow()
         {
             InitializeComponent();
-        
+
+            tablero = new List<Ficha>();
+            blanco = new SolidColorBrush(Colors.White);
+            rojo = new SolidColorBrush(Colors.Red);
+            amarillo = new SolidColorBrush(Colors.Yellow);
+
+
             for (int columna = 0; columna < 7; columna++)
             {
 
                 for (int fila = 0; fila < 6; fila++)
                 {
-                    fichin = new Ellipse();
-                    fichin.Fill = new SolidColorBrush(Colors.White);
-                    fichin.Stroke = new SolidColorBrush(Colors.Black);
-                    fichin.StrokeThickness = 2;
-                    fichin.MouseDown += new MouseButtonEventHandler(click);
-                    Grid.SetRow(fichin, fila);
-                    Grid.SetColumn(fichin, columna);
-                    grilla.Children.Add(fichin);
+                    fichin = new Ficha();
+                    fichin.puntos.X = columna;
+                    fichin.Columna = columna;
+                    fichin.Fila = fila;
+                    fichin.forma.MouseDown += new MouseButtonEventHandler(click);
+                    fichin.Nombre = "Ficha" + columna.ToString() + fila.ToString();
+                    Grid.SetRow(fichin.forma, fila);
+                    Grid.SetColumn(fichin.forma, columna);
+                    grilla.Children.Add(fichin.forma);
+                    tablero.Add(fichin);
                 }
                 
             }
 
         }
 
+        private bool Marca(Ficha ficha)
+        {
+            if(ficha.jugador == Ficha.Jugador.blanco)
+            {
+                if (turno)
+                {
+                    ficha.forma.Fill = rojo;
+                    ficha.jugador = Ficha.Jugador.rojo;
+                    turno = false;
+                }
+                else
+                {
+                    ficha.forma.Fill = amarillo;
+                    ficha.jugador = Ficha.Jugador.amarillo;
+                    turno = true;
+                }
+                return true;
+            }
+            return false;
+            
+        }
+
         private void click(object sender, MouseButtonEventArgs e)
         {
+            var forma_click = sender as Ellipse; //quien llama
+            int columna = Grid.GetColumn(forma_click); //obtener columna
+            int fila = Grid.GetRow(forma_click);  //obtener fila
+
+            foreach (Ficha ficha in tablero.Reverse<Ficha>()) //iteraccion invertida para que se coloreen los ultimos primero
+            {
+                if (ficha.Columna == columna)   //si no esta en la columna, sigue
+                {
+                    if (Marca(ficha))
+                    {
+                        break;
+                    }
+                    
+                }
+               
+            }
+
+            //chequear si hay ganador
+            int contador = 0;
+
             
-            var blanco = new SolidColorBrush(Colors.White);
-            var rojo = new SolidColorBrush(Colors.Red);
-            var amarillo = new SolidColorBrush(Colors.Yellow);
 
-            var ficha = sender as Ellipse; //quien llama
+            foreach (Ficha ficha in tablero)
+            {
+                tabla[ficha.Fila, ficha.Columna] = ficha.jugador.ToString();
+            }
 
-            int columna = Grid.GetColumn(ficha); //obtener columna
-            int fila = Grid.GetRow(ficha);  //obtener fila
-
-
-            //debug
-            label1.Content = columna;
-            label2.Content = fila;
-            label3.Content = ficha.Fill;
-
-
-            
-            
+            //ss
 
 
 
